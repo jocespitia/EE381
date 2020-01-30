3a56ae4d860b2464837500a6df500ab7aa2a5fae
jocespitia <jocelyn.espitia@gmail.com>
2020-01-30 12:36:48 -0800

    finished project 1. Random Number Generator with die rolls and coin tosses

--- Project 1/project1.py
+++ Project 1/project1.py
@@ -8,37 +8,44 @@ End Date:
 Description: The code implements a linear congruential random number generator, RNG. It will output a uniform 
 distribution of numbers on the interval [0, 1).
 '''
-
-def main():
+import math
     
-    def RNG():
-        r= [] # list of random numbers
-        #below are the constants used in the formula
-        N = 10000 #The norm
-        A = 4857 #The adder
-        M = 8601 #The multiplier
-        # -------------------------------------------
+def RNG():
+    r= [] # list of random numbers
+    #below are the constants used in the formula
+    N = 10000 #The norm
+    A = 4857 #The adder
+    M = 8601 #The multiplier
+    # -------------------------------------------
 
-        import time
-        S = time.time() - time.process_time()
-        #---------------------------------------------
+    import time
+    S = time.perf_counter()
+    #---------------------------------------------
 
-        for k in range(25):
-            S = (M * S + A) % N #formula for the RNG
-            v = S / N #r is a decimal number in [0, 1)
-            r.append(v)
-        return r
+    for k in range(25):
+        S = (M * S + A) % N #formula for the RNG
+        v = S / N #r is a decimal number in [0, 1)
+        r.append(v)
+    return r
 
-    def die(r):
-        import math
-        print("Die Roll: ")
-        for k in range(25):
-            die = math.floor(6*r[k] + 1)
-            print(die)
-            
+def die(r):
+    print("Die Roll: ")
+    for k in range(25):
+        die = math.floor(6*r[k] + 1)
+        print(die)
+        
+def coin(r):
+    print("Coin Toss: ")
+    for k in range(25):
+        coin = math.floor(2*r[k])
+        if coin == 0:
+            print("Tails")
+        else:
+            print("Heads")
+        
 
-    r = RNG()
-    die(r)
+r = RNG()
+die(r)
+print()
+coin(r)
     
-
-main()
